37ab2109bcf646fab320ad836e0bdeafc9b1e8a9
Xpecialist <manolouk2010@gmail.com>
2022-05-24 20:03:54 +0300

    commit

--- Παραδοτέο 4/Code/Classes/inventory.py
+++ Παραδοτέο 4/Code/Classes/inventory.py
@@ -1,7 +1,6 @@
 class Inventory:
-    def __init__(self,materialList):
-        self.coins = 0
-        self.size = 10
-        self.materialList = materialList
-        self.itemList = []
-        self.coins = 0
+    def __init__(self):
+        self.__coins = 0
+        self.__size = 10
+        self.__materialList = ['cables','gears','metalScraps','oil','springs']
+        self.__itemList = []
