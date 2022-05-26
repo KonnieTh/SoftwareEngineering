ae4ee70ac34b243917a8751966197897cb2bc0f6
KonnieTh <konstantinathanop1@gmail.com>
2022-05-26 20:50:43 +0300

    commit

---
+++ Παραδοτέο 5/code/inventory.py
@@ -0,0 +1,6 @@
+class Inventory:
+    def __init__(self):
+        self.__coins = 0
+        self.__size = 10
+        self.__materialList = ['cables','gears','metalScraps','oil','springs']
+        self.__itemList = []
