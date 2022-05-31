107ca5f41a9874c68348ed9483efa79ae7c2c70e
KonnieTh <konstantinathanop1@gmail.com>
2022-06-01 00:27:43 +0300

    commit

---
+++ Code/Classes/Blueprint.py
@@ -0,0 +1,24 @@
+class Blueprint():
+    def __init__(self,itemTag):
+        self.__list_of_materials = []
+        self.itemTag=itemTag
+    
+        if(self.itemTag=="gum_gun"):
+           self.__list_of_materials.append("")
+        if(self.itemTag=="stun_gun"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="EMP_grenade"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="time_travel_grenade"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="time_freeze_grenade"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="life_potion"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="speed_potion"):
+            self.__list_of_materials.append("")
+        if(self.itemTag=="stamina_potion"):
+            self.__list_of_materials.append("")
+    
+    def getBlueprint(self):
+        return  self.__list_of_materials
--- Code/Classes/inventory.py
+++ Code/Classes/inventory.py
@@ -1,51 +1,56 @@
+from Blueprint import Blueprint
+
 class Inventory:
     def __init__(self):
         self.__coins = 0
         self.__size = 10
-        self.revival_token=0
+        self.pos={1:'',2:'',3:'',4:''} #positions to place blueprints
         self.__materialDict = {'cables':0,'gears':0,'metalScraps':0,'oil':0,'springs':0}
         self.__itemDict = {'gum_gun':0,'stun_gun':0,'EMP_grenade':0,'time_travel_grenade':0,'time_freeze_grenade':0,'life_potion':0,'speed_potion':0,'stamina_potion':0}
     
-    def craft(self,item): #craft item by calling craft(item_name)
-        pass
-
-    def getBlueprints(self):
-        pass
-
-    def doesPlayerHaveEnoughCoins(self):
-        pass
+    def doesPlayerHaveEnoughCoins(self,num): #returns True if player has enough coins
+        if(self.__coins>=num):
+            return True
+        else:
+            return False
     
-    def isThereAnotherItemInPos(self):
-        pass
+    def isThereAnotherItemInPos(self,position): #returns True if there is another item in the chosen position
+        if(self.pos[position]!=''):
+            return True
+        else:
+            return False
 
-    def removeItem(self,item): #remove an item
+    def removeItem(self,item): #removes 1 of the given item
         if(self.__itemDict[item]>0):
             self.__itemDict[item]-=1
         else:
             print("ERROR")
 
-    def checkIfMaterialsAreMissing(self,material,num):
-        if(self.__materialDict[material]>=num):
-            return True
-        else:
-            return False
+    def checkIfMaterialsAreMissing(self,item): #returns True if there are enough materials for the item to be made
+        blueprint=Blueprint(item)
+        lis=blueprint.getBlueprint() #list of materials the item consists of
+        for i in lis:
+            if self.__materialDict[i]==0:
+                return False
+        return True
 
-    def addItem(self,item): #add an item
+    def addItem(self,item): #adds 1 of the given item
         self.__itemDict[item]+=1
 
-    def removeMaterials(self,material,num): #remove a specific amount of a material
-        if(self.__materialDict[material]>=num):
-            self.__materialDict[material]-=num
-        else:
-            print('Error')
+    def removeMaterials(self,item): #removes the materials the item consists of
+        blueprint=Blueprint(item)
+        lis=blueprint.getBlueprint() #list of materials the item consists of
+        for i in lis:
+            self.__materialDict[i]-=1
 
-    def dismantle(self,item):
-        if(self.__itemDict[item]==0):
-            return False
-        else:
-            self.__itemDict[item]-=1
+    def dismantle(self,item): #dismantles an item
+        blueprint=Blueprint(item)
+        lis=blueprint.getBlueprint() #list of materials the item consists of
+        for i in lis:
+            self.__materialDict[i]+=1
+        self.removeItem(item)
 
-    def enoughSpace(self):
+    def enoughSpace(self): #returns True if there is enough space in the inventory
         spaces=0
         for i in self.__materialDict:
             if self.__materialDict[i]>0:
@@ -58,12 +63,11 @@ class Inventory:
         else:
             return False
 
-    def canPlayerCraftNewItem(self):
-        pass
+    def canPlayerCraftNewItem(self): #returns True if after the addition of some materials the player can craft a new item
+        for item in self.__itemDict:
+            if self.checkIfMaterialsAreMissing(item):
+                return True
 
-    def addMaterials(self,material,num): #add a specific amount of a material
+    def addMaterials(self,material,num): #adds a specific amount of a material
         self.__materialDict[material]+=num
-
-
-    
 
