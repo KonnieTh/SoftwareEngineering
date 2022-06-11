c0656c15b6eb2d6b2584a8b7fb752f3946d28a3c
dorakrem <dorakrem@gmail.com>
2022-06-11 20:50:15 +0300

    commit

--- Code/Classes/Hint.py
+++ Code/Classes/Hint.py
@@ -1,11 +1,14 @@
 class Hint:
-    def __init__(self,hintTitle,tags,hintDescription):
+    def __init__(self,hintTitle,tags,hintDescription,creator):
         self.hintTitle = hintTitle
         self.tags = tags
         self.hintDescription = hintDescription
         self.likes = 0
         self.dislikes = 0
+        self.creator = creator # creator must be of type User
 
 
     def getHint(self):
         return self.hintTitle, self.tags, self.hintDescription
+
+
---
+++ Code/Classes/hint-see-create.py
@@ -0,0 +1,54 @@
+from User import User 
+from Hint import Hint
+from ServerConnection import ServerConnection
+ 
+ 
+#create a user and a server instance
+user1 = User("user1","jejw","usr@gmail.com")
+user1.numOfHints = 2
+server = ServerConnection(None,None, None)
+hint1 = Hint("someHint",None,"someDescription",user1.username)
+
+input1 = input('Do you want to see a hint? Type yes or no. \n')
+
+if input1=="yes":
+    if (user1.checkNumOfHints()==True):
+        hint_input = input('Please type the title of the hint you want to see: \n')
+
+        for i in server.hintDict:
+            if hint_input in server.hintDict:
+                user1.getHint(server.hintDict[i])
+                
+                review_input = input("Please rate this hint. Type yes for a positive review or no for a negative review: \n")
+                
+                if review_input == "yes":
+                    server.hintDict[i].likes += 1
+                    server.updateHintReviews()
+                    server.percentOfPosReviews()
+
+                elif review_input == "no":
+                    server.hintDict[i].likes -= 1
+                    server.updateHintReviews()
+                    server.percentOfPosReviews()
+
+
+            else:
+                print("The hint you typed is not available. \n")
+
+    else: 
+        print("You cannot see a hint. \n")
+
+elif input1 =="no":
+    input2 = input("Do you want to create a hint? Type yes or no. \n")
+    if input2 == "yes":
+            hint_title, hint_description = input("Please type the title and the description of the new hint: ").split()
+            server.hintDict.update({hint_title,hint_description})
+            user1.createHint(hint_title,hint_description,user1.username)
+            print(hint_title)
+    else:
+        print("ERROR")
+
+else:
+    print("ERROR")
+
+
