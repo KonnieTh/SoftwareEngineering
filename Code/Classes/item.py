eb129f81e4124a77f49c340d5f96cba15651c6cb
Xpecialist <manolouk2010@gmail.com>
2022-06-12 21:57:02 +0300

    commit

--- Code/Classes/item.py
+++ Code/Classes/item.py
@@ -1,8 +1,21 @@
+import math
+
 class Item:
-    def __init__(self, value, itemName, description):
+    def __init__(self, value, itemName, description, x_pos, y_pos, pic):
         self.value = value
         self.itemName = itemName
         self.description = description
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.inInv = False
+        self.pic = pic
+
+    def collided(self, alienX):
+        distance = math.sqrt((math.pow(abs(self.x_pos - alienX),2)))
+        if distance < 70:
+            return True
+        else:
+            return False
 
 class MysteryItem(Item):
     #nothing for now
